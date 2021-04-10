e5ff3b9358f76a0d6f75ce21f42ee81017074b4b
linerocks <tirtadwipa.manunggal@gmail.com>
2021-04-10 13:48:41 +0700

    WIP

--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
-import { INestApplication } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { Express } from 'express';
+import { INestApplication } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import { Express } from "express";
 
-import { AppModule } from './src/app.module';
+import { AppModule } from "./src/app.module";
 
-export async function createApp(expressApp: Express): Promise<INestApplication> {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), { cors: true });
+export async function createApp(
+  expressApp: Express
+): Promise<INestApplication> {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+    { cors: true }
+  );
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+    next();
+  });
 
   return app;
 }
